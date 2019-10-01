b40ab464c0009c7ebe5f417e0f3c94f99ec56934
beccaelenzil <beccaelenzil@gmail.com>
2019-10-01 13:25:23 -0700

    @books = Book.all in controller index action

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,6 +1,6 @@
 HIKES = [
-  {id: 1,	name: 'Lake 22',	length_miles: 5.4,	elevation_gain_feet: 1350,	max_elevation_feet: 2400,	rating: 4},
-  {id: 2,	name: "Annette Lake",	length_miles: 7.5,	elevation_gain_feet: 1800,	max_elevation_feet: 3600,	rating: 3}
+  {id: 1,	name: 'Lake 22',	length_miles: 5.4,	elevation_gain_feet: 1350,	max_elevation_feet: 2400,	rating: 4, best_month: "July"},
+  {id: 2,	name: "Annette Lake",	length_miles: 7.5,	elevation_gain_feet: 1800,	max_elevation_feet: 3600,	rating: 3, best_month: "May"}
 ]
 
 class HikesController < ApplicationController
