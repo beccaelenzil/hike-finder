cf89155fe202aca8a573ffe28abb93af684bc12e
beccaelenzil <beccaelenzil@gmail.com>
2019-10-01 13:06:35 -0700

    Hike model

---
+++ db/migrate/20191001200559_create_hikes.rb
@@ -0,0 +1,14 @@
+class CreateHikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :hikes do |t|
+      t.integer :id
+      t.string :name
+      t.float :length_miles
+      t.integer :elevation_gain_feet
+      t.integer :max_elevation_feet
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
